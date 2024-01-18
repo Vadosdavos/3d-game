96eda09bc4eeceac6072adc36d9d88f8f8d1c194
Vadosdavos <bezbvap@gmail.com>
2024-01-18 23:14:51 +0200

    feat: add first pers view

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import * as RAPIER from "@dimforge/rapier3d-compat";
-import { RigidBody, useRapier } from "@react-three/rapier";
+import { CapsuleCollider, RigidBody, useRapier } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { usePersonControls } from "../hooks/usePersonControls";
@@ -22,7 +22,7 @@ export default function Player() {
     playerRef.current.setLinvel({ x: 0, y: 8, z: 0 });
   };
 
-  useFrame(() => {
+  useFrame((state) => {
     if (!playerRef.current) return;
 
     // moving player
@@ -30,7 +30,10 @@ export default function Player() {
 
     frontVector.set(0, 0, backward - forward);
     sideVector.set(left - right, 0, 0);
-    direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(MOVE_SPEED);
+    direction.subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(MOVE_SPEED)
+      .applyEuler(state.camera.rotation);
 
     playerRef.current.wakeUp();
     playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
@@ -41,15 +44,20 @@ export default function Player() {
       playerRef.current.translation(),
       { x: 0, y: -1, z: 0 },
     ));
-    const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1;
+    const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.5;
 
     if (jump && grounded) doJump();
+
+    // moving camera
+    const { x, y, z } = playerRef.current.translation();
+    state.camera.position.set(x, y, z);
   });
 
   return (
-    <RigidBody position={[0, 1, -2]} mass={1} ref={playerRef} lockRotations>
+    <RigidBody colliders={false} position={[0, 1, -2]} mass={1} ref={playerRef} lockRotations>
       <mesh>
         <capsuleGeometry args={[0.5, 0.5]} />
+        <CapsuleCollider args={[0.75, 0.5]} />
       </mesh>
     </RigidBody>
   );
